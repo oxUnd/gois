88945204ce1061fcca0c3e40333cc95f9d32668d
xiangshouding <fansekey@gmail.com>
2013-10-30 13:06:31 +0800

    update

--- gois.js
+++ gois.js
@@ -3,7 +3,6 @@ var fis = module.exports = require('fis');
 fis.cli.name = "gois";
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
-var TEMPLATE_DIR = process.env['BEEGO_APP_DIR'] || "";
 
 fis.config.merge({
     modules : {
@@ -30,18 +29,18 @@ fis.config.merge({
                 isMod : true,
                 id : '${namespace}/widget/$1',
                 url : '${namespace}/widget/$1',
-                release : TEMPLATE_DIR + '/view/${namespace}/widget/$1'
+                release : '/view/${namespace}/widget/$1'
             },
             {
                 reg : /^\/widget\/(.*\.(js|css))$/i,
                 isMod : true,
-                release : TEMPLATE_DIR + '/static/${namespace}/widget/$1'
+                release : '/static/${namespace}/widget/$1'
             },
             {
                 reg : /^\/page\/(.+\.tpl)$/i,
                 isMod: true,
                 id : '${namespace}/widget/$1',
-                release : TEMPLATE_DIR + '/view/${namespace}/page/$1',
+                release : '/view/${namespace}/page/$1',
                 extras: {
                     isPage: true
                 }
@@ -52,14 +51,14 @@ fis.config.merge({
             },
             {
                 reg: /^\/(static|config|test)\/(.*)/i,
-                release: TEMPLATE_DIR + '/$1/${namespace}/$2'
+                release: '/$1/${namespace}/$2'
             },
             {
                 reg : /^\/(plugin|server\.conf$|smarty\.conf$)|\.php$/i
             },
             {
                 reg: "domain.conf",
-                release: TEMPLATE_DIR + '/config/$&'
+                release: '/conf/$&'
             },
             {
                 reg: "build.sh",
@@ -67,11 +66,11 @@ fis.config.merge({
             },
             {
                 reg : '${namespace}-map.json',
-                release : TEMPLATE_DIR + '/config/${namespace}-map.json'
+                release : '/views/config/${namespace}-map.json'
             },
             {
                 reg: /^.+$/,
-                release: TEMPLATE_DIR + '/static/${namespace}$&'
+                release: '/static/${namespace}$&'
             }
         ]
     },
